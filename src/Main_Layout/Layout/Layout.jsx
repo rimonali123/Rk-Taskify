31902f9404268f2290555ebeec7eca51f53f857c
rimoali123 <rimonali450@gmail.com>
2025-01-16 23:35:05 +0600

    Footer section added

--- src/Main_Layout/Layout/Layout.jsx
+++ src/Main_Layout/Layout/Layout.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../../Components/Shared-Components/Header/Navbar';
+import Footer from '../../Components/Footer/Footer';
 
 const Layout = () => {
     return (
-        <div className='mx-0 max-w-7xl'>
-          <Navbar></Navbar>
-            <Outlet></Outlet>
-           
+        <div>
+            <div className='mx-0 max-w-7xl'>
+                <Navbar></Navbar>
+                <Outlet></Outlet>
+
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
