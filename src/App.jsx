3732fbed8e086ea38fccd64f6d59cf52b979eb14
rimoali123 <rimonali450@gmail.com>
2025-01-16 20:55:51 +0600

    Root setup successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { router } from './Root/Root'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello word ...... 😍💕 
+      <RouterProvider router={router} />
     </>
   )
 }
